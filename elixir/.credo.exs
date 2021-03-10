d61038390311d579c7fda8f6ccaa4b070c082912
Trevor Brown <trevorbrown@rentpath.com>
2021-03-10 10:30:59 -0500

    feat!: run credo on test .ex and .exs files
    We often make simple style guide violations in our test code that can be
    caught by Credo. Running Credo for tests should result in better
    formatting for our test code.

--- elixir/.credo.exs
+++ elixir/.credo.exs
@@ -19,7 +19,7 @@
         #
         # you can give explicit globs or simply directories
         # in the latter case `**/*.{ex,exs}` will be used
-        included: ["lib/", "src/", "test/", "web/", "apps/"],
+        included: ["lib/", "src/", "web/", "apps/", "test/**/*.{ex,exs}"],
         excluded: [~r"/_build/", ~r"/deps/", ~r"/node_modules/"]
       },
       #
